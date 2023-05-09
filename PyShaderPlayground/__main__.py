41640eae50df37b5bfdecfe258b72123b314b638
utak3r <piotr.borys@gmail.com>
2023-05-09 11:25:41 +0200

    Stop using deprecated exec_()

--- PyShaderPlayground/__main__.py
+++ PyShaderPlayground/__main__.py
@@ -9,6 +9,12 @@ from PySide6.QtGui import QIcon
 from PyShaderPlayground.main_window import ShaderPlayground
 
 if __name__ == "__main__":
+
+    dirname = os.path.dirname(PySide6.__file__)
+    #dirname = 'g:\\Python\\Qt6\\Library'
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon('app.ico'))
@@ -16,4 +22,4 @@ if __name__ == "__main__":
     mainWnd = ShaderPlayground()
     mainWnd.show()
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
